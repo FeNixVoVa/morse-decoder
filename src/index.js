7625ac3bbc1a8d92462ddee7d5b59c64d4d1bb37
FeNixVoVa <FeNixVoVa@gmail.com>
2020-10-21 20:51:19 +0300

    feat: complete task Morse Decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,26 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let stringSize = expr.length;
+    let result = '';
+    for (let i = 0; i < stringSize; i = i + 10) {
+        let charString = expr.slice(i, i + 10);
+        if (charString === '**********') {
+            result += ' ';
+        } else {
+            let someChar = '';
+            for (let j = 0; j < 10; j = j + 2) {
+                let partChar = charString.slice(j, j + 2);
+                if (partChar === '11') {
+                    someChar += '-';
+                } else if (partChar === '10') {
+                    someChar += '.';
+                }
+            };
+            result += MORSE_TABLE[someChar.toString()];
+        }
+    }
+    return result;
 }
 
 module.exports = {
